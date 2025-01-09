4229ed277da41fe3dcbf4992794a439a62aece3f
Karto-manikandan <manikandan.r@kartoffel.in>
2025-01-09 22:47:47 +0530

    Initial Commit

---
+++ FileManagerPlus/FileManagerPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerPlusApp.swift
+//  FileManagerPlus
+//
+//  Created by MANIKANDAN on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
